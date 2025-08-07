ff88619f3417d57b41946022ab1ad0a691a65f67
DARGA DIWAKAR <dargadiwakar@gmail.com>
2025-08-07 09:52:03 +0530

    Add files via upload

---
+++ java script/projectcurrency.js
@@ -0,0 +1,6 @@
+let rupees=document.getElementById("rupees");
+let result=document.getElementById("result");
+function convert(){
+    // let dollers=
+    result.textContent=parseInt(rupees.value)*0.12;
+};
